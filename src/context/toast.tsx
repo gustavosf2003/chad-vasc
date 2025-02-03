37eadc02cdab6db5ba3f0828e2c4a7378a62396a
GustavoQueenslab <gustavo@queenslab.pt>
2025-02-03 17:39:16 -0300

    fix: ts error

--- src/context/toast.tsx
+++ src/context/toast.tsx
@@ -1,14 +1,22 @@
+import { ShowToastType } from "@/types/Toast";
 import React, { createContext, useContext } from "react";
 
 import { ToastContainer, toast } from "react-toastify";
 
-import { ShowToastType, TOAST_TYPES } from "@/types/toast";
-
 import "react-toastify/dist/ReactToastify.css";
 
-const ToastContext = createContext(undefined);
+// Define the type for the context value
+interface ToastContextType {
+  showToast: (
+    type: string,
+    message: string,
+    options?: { autoClose: number }
+  ) => ShowToastType;
+}
 
-export function ToastProvider({ children }) {
+const ToastContext = createContext<ToastContextType | undefined>(undefined);
+
+export function ToastProvider({ children }: { children: React.ReactNode }) {
   function showToast(
     type: string,
     message: string,
@@ -16,6 +24,7 @@ export function ToastProvider({ children }) {
       autoClose: 2000,
     }
   ): ShowToastType {
+    // @ts-expect-error - showToast is not defined
     const toastFn = TOAST_TYPES[type] || toast.success;
     return toastFn(message, options);
   }
